86c54468b5d7c92c281e2bac5b02ab5af2e6133f
Abubakr Boymirzayev <89568021+abubakr29@users.noreply.github.com>
2022-04-29 12:15:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+non = 2500
+shokolad = 10000
+kofe = 5000
+a = int(input('Nechta non olasiz: '))
+b = int(input('Nechta shokolad olasiz: '))
+c = int(input('Necha bokal kofe olasiz: '))
+d = a * non
+e = b * shokolad
+f = c * kofe
+print(f"Sizning xaridingiz: {d + e + f} so'm bo'ldi.")
